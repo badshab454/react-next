9c15f92c19070eae6743756da9ff50fdfb59bba2
Badsha Hossain <badshab454@gmail.com>
2023-08-28 13:14:36 +0600

    useState hook

--- app/page.js
+++ app/page.js
@@ -1,13 +1,14 @@
 "use client";
 import React, {useState} from 'react';
 
-
 const page = () => {
-  const [count, setCount] = useState(0)
-  return <>
-    <p>You clicked {count} times.</p>
-    <button onClick={() => setCount(count+1)}>Click Me</button>
-  </>
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+      <p>You clicked {count} times.</p>
+      <button onClick={() => setCount(count+1)}>click me</button>
+    </div>
+  )
 }
 
 export default page;
